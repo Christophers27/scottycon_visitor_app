68a623acb098f1659a960708a1a0e72a35692ca2
Christophers27 <christophersetiabudi@gmail.com>
2023-05-13 17:09:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scottycon_visitor_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/scottycon_visitor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scottycon_visitor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
